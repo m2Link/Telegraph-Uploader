e6b4675077f3a544c292f59c44b2c07d43534b8f
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-14 21:36:58 +0530

    Update telegraph.py

--- TelegraphBot/telegraph.py
+++ TelegraphBot/telegraph.py
@@ -79,3 +79,12 @@ async def upload(file):
     else:
         url = "https://telegra.ph" + response[0].get("src")
     return url
+#Telegraph
+@Client.on_message(filters.private & filters.incoming & filters.command("help"))
+async def _help(bot, msg):
+    await bot.send_message(
+        msg.chat.id,
+        Data.HELP,
+        reply_markup=InlineKeyboardMarkup(Data.home_buttons),
+        reply_to_message_id=msg.message_id
+    )
